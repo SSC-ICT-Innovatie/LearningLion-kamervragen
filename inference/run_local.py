7d3a595c0e6ba9b6b920de0018d417a49e5b6234
Mark Heijnekamp <struiken_klieven.0u@icloud.com>
2024-11-27 14:46:25 +0100

    Fix: device is not a generation kwarg

--- inference/run_local.py
+++ inference/run_local.py
@@ -14,19 +14,6 @@ def infer_run_local(prompt,
   if inference_Global is None or inference_Global.llm != LLM:
     if LLM in gpuBanlist:
       inference_Global = Infer(LLM, exclude_device=True)
-  # Models that dont require a GPU or are needed to exclude the device tag
-
-  device = "cpu"
-  if not torch.cuda.is_available():
-    device = "cpu"
-  if torch.backends.mps.is_available():
-    device = "mps"
-  if torch.cuda.is_available():
-    device = "cuda"
-    
-  if LLM not in gpuBanlist:
-    generation_kwargs = generation_kwargs or {}
-    generation_kwargs["device"] = device
   
   return inference_Global.predict(prompt, chatlog, files, systemPrompt, generation_kwargs)
 
